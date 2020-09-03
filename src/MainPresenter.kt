781d7cc5c2966aead996104f2f123de1d9e26919
kevinsarosa <kevinsarosa@gmail.com>
2020-09-03 13:06:46 +0700

    perbaikan logic

--- src/MainPresenter.kt
+++ src/MainPresenter.kt
@@ -5,12 +5,29 @@ class MainPresenter(var view: Interface.View) : Interface.Presenter {
 
 
     override fun process(playerChoice: Int, computerChoice: Int, randomNumber: Int) {
+
+        var selisihPlayer = 0
+        var selisihCom = 0
+
+        if (playerChoice > randomNumber){
+            selisihPlayer = playerChoice-randomNumber
+        }else if (randomNumber> playerChoice){
+            selisihPlayer = randomNumber-playerChoice
+        }
+
+        if (computerChoice>randomNumber){
+            selisihCom = computerChoice-randomNumber
+        }else if(randomNumber> computerChoice){
+            selisihCom = randomNumber-computerChoice
+        }
+
+
         when {
-            computerChoice == randomNumber -> {
+            selisihCom<selisihPlayer -> {
                 result = 1
                 println("Computer Win")
             }
-            playerChoice == randomNumber -> {
+            selisihPlayer<selisihCom -> {
                 result = 2
                 println("Player Win")
             }
@@ -21,9 +38,9 @@ class MainPresenter(var view: Interface.View) : Interface.Presenter {
         }
 
         if (result == 2) {
-            playerScore += 1
-        } else if (result == 3) {
-            computerScore += 1
+            ++playerScore
+        } else if (result == 1) {
+            ++computerScore
         }
         view.scoreResult(playerScore,computerScore)
     }
